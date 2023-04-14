832b8f097afee723451380ca8148faca408a1e2e
harshSE <h.rsh89@gmail.com>
2023-04-14 16:18:38 +0530

    First commit

---
+++ src/test/java/com/azureembeddedapp/AzureEmbededAppTest.java
@@ -0,0 +1,21 @@
+package com.azureembeddedapp;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class AzureEmbededAppTest {
+
+  @Inject
+  EmbeddedApplication<?> application;
+
+  @Test
+  void testItWorks() {
+    Assertions.assertTrue(application.isRunning());
+  }
+
+}
