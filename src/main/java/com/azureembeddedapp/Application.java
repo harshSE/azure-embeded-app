0392f5b7deb816e9e28fc0c61a6f62292bd3888c
harshSE <h.rsh89@gmail.com>
2023-08-02 20:34:24 +0530

    make azure monitor by default true, sout for AZUREMONITOR_INSTRUMENTATION_KEY received or not, use gradle in yml file

--- src/main/java/com/azureembeddedapp/Application.java
+++ src/main/java/com/azureembeddedapp/Application.java
@@ -5,6 +5,12 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
   public static void main(String[] args) {
+    if (System.getenv("AZUREMONITOR_INSTRUMENTATION_KEY") != null
+    && !System.getenv("AZUREMONITOR_INSTRUMENTATION_KEY").isBlank()) {
+      System.out.println("===========================AZUREMONITOR_INSTRUMENTATION_KEY received");
+    } else {
+      System.out.println("===========================AZUREMONITOR_INSTRUMENTATION_KEY not received");
+    }
     Micronaut.run(Application.class, args);
   }
 }
